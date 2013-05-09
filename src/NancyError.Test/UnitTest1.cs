61442e56a5a6af01ae1169c098755da149775cdf
Kellen <k@8labs.com>
2013-05-09 16:48:18 -0700

    errors when running the unit tests
    This was created using VS 2012 Express for Desktop

---
+++ src/NancyError.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+using Nancy.Security;
+using Nancy.Testing;
+using NancyError;
+
+namespace NancyError.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var boot = new TestBootstrapper();
+
+            var browser = new Browser(boot);
+
+            var response = browser.Get("/", (with) =>
+            {
+                with.HttpRequest();
+                //with.FormsAuth(session.Id, this.authConfig);
+            });
+
+            var bob = response;
+        }
+    }
+}
---
+++ src/NancyError/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace NancyError
+{
+    public class TestBootstrapper : DefaultNancyBootstrapper
+    {
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var nancyHost = new NancyHost(new TestBootstrapper(), new Uri("http://localhost:8888/testing/"));
+            nancyHost.Start();
+
+            Console.ReadKey();
+
+            nancyHost.Stop();
+
+        }
+    }
+}
